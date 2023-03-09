7fbea5bc86369309405972e0f3e0b592d6b0e9aa
Danil Brudanin <brudankovko@gmail.com>
2023-03-09 22:45:07 +0300

    v. 0.0.1

---
+++ start/script.js
@@ -0,0 +1,8 @@
+const btnStart = document.querySelectorAll('.btn-start')
+const piano1 = document.querySelector('.piano1')
+const pianoOpen = document.querySelector('.piano1-open')
+
+
+btnStart.addEventListener('click', () => {
+    
+})
