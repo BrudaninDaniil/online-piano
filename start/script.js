419d3b96837d654996d576ea59a25e81d1a52467
Danil Brudanin <brudankovko@gmail.com>
2023-03-09 23:35:54 +0300

    v. 0.1.0

--- start/script.js
+++ start/script.js
@@ -1,8 +1,20 @@
-const btnStart = document.querySelectorAll('.btn-start')
-const piano1 = document.querySelector('.piano1')
-const pianoOpen = document.querySelector('.piano1-open')
+const btnStart = document.getElementById('openClosed');
+const pianoClosed = document.getElementById('pianoClosed');
+const pianoOpen = document.getElementById('pianoOpen');
 
+console.log(pianoClosed);
 
-btnStart.addEventListener('click', () => {
-    
+function checkPiano (piano) {
+    piano.style.opacity = 100 + '%' ? true : false ;
+}
+
+pianoOpen.addEventListener('click', () => {
+    if (checkPiano(pianoClosed)) {
+        pianoClosed.style.opacity = 0 + '%'
+        pianoOpen.style.opacity = 100 + '%'
+    }
+    if (checkPiano(pianoOpen)) {
+        pianoOpen.style.opacity = 0 + '%'
+        pianoClosed.style.opacity = 100 + '%'
+    }
 })
